c4950b017f2b25cd01aaa65f1f82f7604d3fc046
IreneTayler <Irene19tayler@outlook.com>
2026-03-01 20:03:48 +0300

    footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -76,7 +76,7 @@ const Footer = () => {
         </motion.div>
 
         {/* Middle: Social Icons */}
-        <motion.div
+        {/* <motion.div
           initial={{ opacity: 0, y: 20 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.8, delay: 0.2 }}
@@ -110,7 +110,7 @@ const Footer = () => {
           >
             <FaWhatsapp size={24}/>
           </a>
-        </motion.div>
+        </motion.div> */}
 
       </div>
 
